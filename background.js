59ad43fac679d7bae669f3b152aec8efe6d6d5a9
Zhukov Maxim <my.fsou1@gmail.com>
2017-10-13 17:12:55 +0300

    Implement tab's limit functionality

---
+++ background.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    var maxTabsCount = 25;
+
+    var handleTabEvent = function(tab) {
+        if(!tab || !tab.id) return;
+
+        chrome.tabs.query({
+            windowType: 'normal',
+            pinned: false
+        }, function(tabs) {
+            if(tabs.length > maxTabsCount) {
+                alert('Tabs limit is exceed.\r\nYou can not open more than ' + maxTabsCount + ' tabs.\r\nNow opened: ' + tabs.length);
+                chrome.tabs.remove(tab.id);
+            }
+        });
+    };
+    
+    chrome.tabs.onCreated.addListener(handleTabEvent);
+    chrome.tabs.onRemoved.addListener(handleTabEvent);        
+})();
